6ca57e3f95a49b922944915d118d83b9cd718b66
Sarfaraz <khansarfaraz2141@gmail.com>
2021-09-14 09:53:25 +0530

    Perform Classic Updates by Running Find, Edit, then Save

--- myApp.js
+++ myApp.js
@@ -64,7 +64,19 @@ const findPersonById = function (personId, done) {
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  // findById() method
+  Person.findById(personId, (err, person) => {
+    if (err) return console.log(err);
+
+    // Array.push() method
+    person.favoriteFoods.push(foodToAdd);
+
+    // save() method
+    person.save((err, updatedPerson) => {
+      if (err) return console.log(err);
+      done(null, updatedPerson);
+    });
+  });
 };
 
 const findAndUpdate = (personName, done) => {
